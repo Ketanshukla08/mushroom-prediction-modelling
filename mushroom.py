61781c1ad301d96ba1acb60a16d2cc04c59e038a
Ketanshukla08 <32417375+Ketanshukla08@users.noreply.github.com>
2017-10-09 21:46:44 -0700

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -30,28 +30,29 @@ for i in df.columns:
     df[i]=df[i].astype("category",ordered=True, categories=df[i].unique()).cat.codes
 
 #now checking the correlation beetween the sets
+from sklearn.feature_selection import mutual_info_regression
+print(mutual_info_regression(df.ix[:,1::], df.ix[:,0]))
+
 df.corr()
 
+
 # HERE WE CAN SEE THAT 'CLASS' DOES NOT DEPEND ON SOME FEATURES EFFECTIVELY, SO WE WOULD REMOVE THOSE FEATURES
-del X['cap-shape']
 del X['cap-color']
+del X['gill-attachment']
+del X['stalk-shape']
 del X['radius']
 del X['weight']
 del X['veil-type']
-del X['stalk-color-above-ring']
-del X['stalk-color-below-ring']
-del X['ring-type']
-del X['habitat']
+del X['ring-number']
 
-del df['cap-shape']
 del df['cap-color']
+del df['gill-attachment']
+del df['stalk-shape']
 del df['radius']
 del df['weight']
 del df['veil-type']
-del df['stalk-color-above-ring']
-del df['stalk-color-below-ring']
-del df['ring-type']
-del df['habitat']
+del df['ring-number']
+
 
 
 #checking the data via stripplot, to get to know the categories of features,
@@ -64,12 +65,11 @@ for i in df.columns:
     sns.distplot(df[i])
     plt.show()
 
-#here we can see from distribution plot that gill-attachment, gill-spacing and ring-number features have almost constant value over their columns
+#here we can see from distribution plot that veil-color feature have almost constant value over their columns
 #so we can remove these columns
 
-del X['gill-spacing']
-del X['gill-attachment']
-del X['ring-number']
+del X['veil-color']
+
 
 #now making the dummies of categorical features to impliment logistic regression
 #it will give numerical value(1 or 0) coressponding to true or false for each category of every feature
@@ -89,7 +89,7 @@ testY= Y_train.ix[4801:5685]
 from sklearn.linear_model import LogisticRegression
 LR = LogisticRegression()
 LR=LR.fit(trainX,trainY)
-print(LR.score(testX,testY))
+print(100*LR.score(testX,testY))
 
 #we got accuracy of 100% for this set
 #with this much accuracy, no need to further amend, so final ouput
